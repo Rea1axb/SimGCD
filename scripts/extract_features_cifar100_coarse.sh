390f6bbd732572982470d51a0b4d0103b735ceb2
Rea1axb <1527039517@qq.com>
2024-04-24 04:13:01 +0800

    Add transform in extract_feature & Modify coarse_weight

--- scripts/extract_features_cifar100_coarse.sh
+++ scripts/extract_features_cifar100_coarse.sh
@@ -3,16 +3,17 @@
 set -e
 set -x
 
-CUDA_VISIBLE_DEVICES=1 python extract_features.py \
+CUDA_VISIBLE_DEVICES=3 python extract_features.py \
     --model_name 'doublecoarse' \
     --dataset 'cifar100' \
     --setting 'default' \
+    --transform 'imagenet' \
     --batch_size 256 \
     --num_workers 8 \
     --extract_block \
     --extract_block_num 10 \
     --warmup_model_dir '/home/czq/workspace/GCD/SimGCD/outputs/simgcd/log/cifar100/cifar100_default_twohead(DoubleCoarsePrototypesSupclsmqsoftClusterContrastiveSupcontrastiveCoarse)_weight(warmup_30-60_0.0-0.5_cooloff_120-150_0.5-0.2)_fineweight(dynamic)_batchsz(256)_(06.04.2024_|_35.020)/checkpoints/model_30.pt'\
-    --exp_name 'cifar100_default_doublecoarse_(06.04.2024_|_35.020)'
+    --exp_name 'cifar100_default_doublecoarse_imagenet_(06.04.2024_|_35.020)'
 
 # CUDA_VISIBLE_DEVICES=3 python train.py \
 #     --dataset_name 'cifar100' \
